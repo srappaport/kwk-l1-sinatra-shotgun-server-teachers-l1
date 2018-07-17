5c2d1c39ae226d4fbee2b95b47eaf55dbeb9cea4
srappaport <sarah.rappaport@greendot.org>
2018-07-17 17:30:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,9 +15,9 @@ class App < Sinatra::Base
   end
 
   get '/:number1/:number2' do
-    @n1 = params[:number1].to_i
-    @n2 = params[:number2].to_i
-    result = @n1*@n2
-end
+    n1 = params[:number1].to_i
+    n2 = params[:number2].to_i
+    n1*n2
+  end
 
 end
